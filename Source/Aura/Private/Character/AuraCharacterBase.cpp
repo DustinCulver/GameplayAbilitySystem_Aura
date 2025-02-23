9fc143e6d2775e23aa196b3df330a5b3d675a9f6
Dustin Culver <dustin_culver@hotmail.com>
2025-02-23 10:58:37 -0600

    Animation Blueprint Class

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -3,11 +3,18 @@
 
 #include "Character/AuraCharacterBase.h"
 
+#include "GeometryCollection/ManagedArrayTypes.h"
+
 
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
 }
 
 void AAuraCharacterBase::BeginPlay()
