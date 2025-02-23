44da2e58b53f79ef3bf54ad3339014a9e09674ea
Dustin Culver <dustin_culver@hotmail.com>
2025-02-23 13:49:19 -0600

    Aura Player Controller Class

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,22 @@
+// Copyright OneForThat
+
+
+#include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+	
+	
+}
